9bba86769632670fcd8af16b7a42134a49fadc81
fredomkb58 <fredomkb@gmail.com>
2026-02-14 04:10:51 +0100

    Trim whitespace from conditional block items

--- Gabs.min.php
+++ Gabs.min.php
@@ -191,7 +191,7 @@ class Gabs
     // getBoolPart - traitement des parties d'un bloc conditionnel
     private function _getBoolPart_s($aRes, $nNro, $sTemp) {
         foreach ( $aRes as $aItm ) {
-        	$sRemp = (isset($aItm[$nNro])) ? $aItm[$nNro] : '';
+        	$sRemp = (isset($aItm[$nNro])) ? trim($aItm[$nNro]) : '';
             $sTemp = str_replace($aItm[0], $sRemp, $sTemp);
         }
         return $sTemp;
